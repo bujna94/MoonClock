dc174585ba0c71a0b0b6be2664b71a7b5787ad42
Jan Dvorak <dvorakh1997@gmail.com>
2021-12-12 11:36:12 +0100

    add coingecko api link

--- CYCLE/conf.py
+++ CYCLE/conf.py
@@ -1,5 +1,6 @@
 
 #copy this configuration below this comment section and define your currencies, time, sleep time etc.
+#you can find more details of the supported cryptocurrencies at https://www.coingecko.com/en/api/documentation
 """
 conf = [
         {'name' : 'bitcoin',    'value' : 'usd',            'prefix' : '$', 'postfix':'',   'remove_decimal': 0, 'sleep_time' : 30 },
