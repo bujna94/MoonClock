6cf42c6bffc0404b17d1f1132183c2bba820a506
Jan Dvorak <dvorakh1997@gmail.com>
2021-12-12 11:34:33 +0100

    fix DOT to be centered to 54px, add default conf sleep time

--- CYCLE/conf.py
+++ CYCLE/conf.py
@@ -2,10 +2,10 @@
 #copy this configuration below this comment section and define your currencies, time, sleep time etc.
 """
 conf = [
-        {'name' : 'bitcoin',    'value' : 'usd',            'prefix' : '$', 'postfix':'',   'remove_decimal': 0, 'sleep_time' : 1 },
-        {'name' : 'ethereum',   'value' : 'eur',            'prefix' : '',  'postfix':'€',  'remove_decimal': 1, 'sleep_time' : 1 },
-        {'name' : 'polkadot',   'value' : 'eur',            'prefix' : '',  'postfix':'€',  'remove_decimal': 0, 'sleep_time' : 1 },
-        {'name' : 'time',       'value' : 'Europe/Prague',  'prefix' : '',  'postfix':'',   'remove_decimal': 0, 'sleep_time' : 1 },
+        {'name' : 'bitcoin',    'value' : 'usd',            'prefix' : '$', 'postfix':'',   'remove_decimal': 0, 'sleep_time' : 30 },
+        {'name' : 'ethereum',   'value' : 'eur',            'prefix' : '',  'postfix':'€',  'remove_decimal': 1, 'sleep_time' : 30 },
+        {'name' : 'polkadot',   'value' : 'eur',            'prefix' : '',  'postfix':'€',  'remove_decimal': 0, 'sleep_time' : 30 },
+        {'name' : 'time',       'value' : 'Europe/Prague',  'prefix' : '',  'postfix':'',   'remove_decimal': 0, 'sleep_time' : 30 },
 ]
 
 """
@@ -13,5 +13,5 @@ conf = [
 conf = [
         {'name' : 'bitcoin',    'value' : 'usd',            'prefix' : '$', 'postfix':'',   'remove_decimal': 1, 'sleep_time' : 30 },
         {'name' : 'time',       'value' : 'Europe/Prague',  'prefix' : '',  'postfix':'',   'remove_decimal': 0, 'sleep_time' : 30 },
-        {'name' : 'polkadot',   'value' : 'eur',            'prefix' : '',  'postfix':'€',  'remove_decimal': 0, 'sleep_time' : 1 },
+        {'name' : 'polkadot',   'value' : 'eur',            'prefix' : '',  'postfix':'€',  'remove_decimal': 0, 'sleep_time' : 30 },
 ]
