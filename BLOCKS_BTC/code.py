3e0d7540b82f4cb011b546b972881ef323f8dacb
Jan Dvorak <dvorakh1997@gmail.com>
2021-12-12 02:00:50 +0100

    rotate time, btc, eth, ada

--- BLOCKS_BTC/code.py
+++ BLOCKS_BTC/code.py
@@ -16,7 +16,6 @@ import adafruit_requests
 
 # URLs to fetch from
 URL_BLOCKHEIGHT = "https://mempool.space/api/blocks/tip/height"
-URL_PRICE = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 URL_PENDING_TXS = "https://bitcoinexplorer.org/api/mempool/count"
 
 # Get wifi details and more from a secrets.py file
@@ -92,136 +91,23 @@ def decide(number):
         return NINE
     elif number == str('.'):
         return DOT
-        print('THIS IS DOT')
+    elif number == str('$'):
+        return DOLLAR
+    elif number == str(':'):
+        return LARGECOLON
+    else:
+        return EMPTY
+        print ('this is empty or unsupported char')
 
 
-cycle = 0
-cases = 2
-while True:
+def string_to_display (string, colon):
+    centered_string = center ( string )
 
-    string = "0"
-    #print("Fetching json from", URL)
-    #response = requests.get(URL)
-    #a = response.json()
-    #btc_price = a['bitcoin']['usd']
-
-    #print('This is BTC price: ' + str(btc_price))
-    #string = str(btc_price)
-    if cycle == 0:
-        try:
-            print ("Fetching block height from", URL_BLOCKHEIGHT)
-            response = requests.get(URL_BLOCKHEIGHT)
-
-            #decode from b'111111'
-            string = response.content.decode()
-            print('This is block height: ' + string)
-        except:
-            string = string
-    elif cycle == 1:
-        try:
-            print("Fetching json from", URL_PRICE)
-            response = requests.get(URL_PRICE)
-            a = response.json()
-            btc_price = a['bitcoin']['usd']
-            string = str(btc_price)
-            print('This is BTC price: ' + string )
-        except:
-            string = string
-    elif cycle == 2:
-        try:
-            print ("Fetching pending txs from", URL_PENDING_TXS)
-            response = requests.get(URL_PENDING_TXS)
-
-            #decode from b'111111'
-            string = str (response.content.decode() ) 
-            print('This is number of pending txs: ' + string )
-        except:
-            string = string
-    
-    print ("String : " + string )
-    try:
-        FIRST = decide(string[-1])
-        print('FIRST working')
-    except:
-        FIRST = EMPTY
-        print('FIRST=EMPTY')
-
-    try:
-        SECOND = decide(string[-2])
-        print('SECOND working')
-    except:
-        SECOND = EMPTY
-        print('SECOND=EMPTY')
-
-    try:
-        THIRD = decide(string[-3])
-        print('THIRD working')
-    except:
-        THIRD = EMPTY
-        print('THIRD=EMPTY')
-
-    try:
-        FOURTH = decide(string[-4])
-        print('FOURTH working')
-    except:
-        FOURTH = EMPTY
-        print('FOURTH=EMPTY')
-
-    try:
-        FIFTH = decide(string[-5])
-        print('FIFTH working')
-    except:
-        FIFTH = EMPTY
-        print('FIFTH=EMPTY')
-
-    try:
-        SIXTH = decide(string[-6])
-        print('SIXTH working')
-    except:
-        SIXTH = EMPTY
-        print('SIXTH=EMPTY')
-
-
-    display1.fill(0) # Clear the display
-    display2.fill(0) # Clear the display
-    display3.fill(0) # Clear the display
-    display4.fill(0) # Clear the display
-    display5.fill(0) # Clear the display
-
-        #for y, row in enumerate(DOLLAR):
-        #    for x, c in enumerate(row):
-        #        display1.pixel(x + 0, y + 0, c)
-
-    if cycle == 1:
-        for y, row in enumerate(DOLLAR):
-            for x, c in enumerate(row):
-                display2.pixel(x - 35, y + 0, c)
-    elif cycle == 0:
-        for y, row in enumerate(SIXTH):
-            for x, c in enumerate(row):
-                display2.pixel(x - 5, y + 0, c)
-
-    for y, row in enumerate(FIFTH):
-        for x, c in enumerate(row):
-            display2.pixel(x + 75, y + 0, c)
-
-    for y, row in enumerate(FOURTH):
-        for x, c in enumerate(row):
-            display3.pixel(x - 5, y + 0, c)
-    for y, row in enumerate(THIRD):
-        for x, c in enumerate(row):
-            display3.pixel(x + 75, y + 0, c)
-
-    for y, row in enumerate(SECOND):
-        for x, c in enumerate(row):
-            display4.pixel(x - 5, y + 0, c)
-    for y, row in enumerate(FIRST):
-        for x, c in enumerate(row):
-            display4.pixel(x + 75, y + 0, c)
-
-    #for y, row in enumerate(BTC):
-    #    for x, c in enumerate(row):
-    #        display5.pixel(x + 0, y + 0, c)
+    print_display (display1, centered_string[0], centered_string[1]);
+    print_display (display2, centered_string[2], centered_string[3]);
+    print_display (display3, centered_string[4], centered_string[5]);
+    print_display (display4, centered_string[6], centered_string[7]);
+    print_display (display5, centered_string[8], centered_string[9]);
 
     display1.show()
     display2.show()
@@ -229,14 +115,163 @@ while True:
     display4.show()
     display5.show()
 
-    print ( 'Cycle: ' + str(cycle) + ' done')
+    return 0
+
+def time_to_display (string, colon):
+    centered_string = center ( string )
+
+    print_time (display1, centered_string[0], centered_string[1], 0);
+    print_time (display2, centered_string[2], centered_string[3], 0);
+    print_time (display3, centered_string[4], centered_string[5], 1);
+    print_time (display4, centered_string[6], centered_string[7], 0);
+    print_time (display5, centered_string[8], centered_string[9], 0);
+
+    display1.show()
+    display2.show()
+    display3.show()
+    display4.show()
+    display5.show()
+
+    return 0
 
 
-    if ( cycle < cases ):
-        cycle+=1;
-    elif ( cycle == cases ):
-        cycle = 0;
+
+def print_time (display, l1, l2, colon_boolean):
+    bin_l1 = decide(l1)
+    bin_l2 = decide(l2)
+
+    display.fill(0) # Clear the display
+    if colon_boolean:
+        for y, row in enumerate(LARGECOLON):
+            for x, c in enumerate(row):
+                display.pixel(x + 0, y + 0, c)
+
+    for y, row in enumerate(bin_l1):
+        for x, c in enumerate(row):
+            if (c == 1):
+                display.pixel(x - 5, y + 0, c)
+
+    for y, row in enumerate(bin_l2):
+        for x, c in enumerate(row):
+            if (c == 1):
+                display.pixel(x + 75, y + 0, c)
+
+    return 0
 
 
-    time.sleep(1)
+def print_display (display, l1, l2):
+    bin_l1 = decide(l1)
+    bin_l2 = decide(l2)
+
+    display.fill(0) # Clear the display
+
+    for y, row in enumerate(bin_l1):
+        for x, c in enumerate(row):
+            if (c == 1):
+                display.pixel(x - 5, y + 0, c)
+
+    for y, row in enumerate(bin_l2):
+        for x, c in enumerate(row):
+            if (c == 1):
+                display.pixel(x + 75, y + 0, c)
+
+    return 0
+
+
+def center (string):
+    length = len(string)
+    print ( "Length of string is " + str( len(string) ) )
+    if (length == 10):
+        return string
+    elif (length == 9):
+        return " " + string
+    elif (length == 8):
+        return " " + string + " "
+    elif (length == 7):
+        return "  " + string + " "
+    elif (length == 6):
+        return "  " + string + "  "
+    elif (length == 5):
+        return "   " + string + "  "
+    elif (length == 4):
+        return "   " + string + "   "
+    elif (length == 3):
+        return "    " + string + "   "
+    elif (length == 2):
+        return "    " + string + "    "
+    elif (length == 1):
+        return "     " + string + "    "
+    elif (length == 0):
+        return "     " + string + "     "
+
+    
+def get_btc_price ():
+
+    URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+
+    print("Fetching json from", URL)
+    response = requests.get(URL)
+    a = response.json()
+    btc_price = a['bitcoin']['usd']
+
+    print('This is BTC price: ' + str(int(btc_price)))
+    string = str(int(btc_price))
+    return string
+
+def get_eth_price ():
+
+    URL = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+
+    print("Fetching json from", URL)
+    response = requests.get(URL)
+    a = response.json()
+    eth_price = a['ethereum']['usd']
+
+    print('This is ETH price: ' + str(int(eth_price)))
+    string = str(int(eth_price))
+    return string
+
+def get_ada_price ():
+
+    URL = "https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd"
+
+    print("Fetching json from", URL)
+    response = requests.get(URL)
+    a = response.json()
+    price = a['cardano']['usd']
+
+    print('This is ADA price: ' + str(price))
+    string = str(price)
+    return string
+
+def get_prague_time ():
+    URL = "http://worldtimeapi.org/api/timezone/Europe/Prague"
+
+    print("Fetching json from", URL)
+    response = requests.get(URL)
+    a = response.json()
+    value = a['datetime']
+
+    string = value[11:13] + value[14:16]
+    print('This is DATE value: ' +  value[11:13] + "separator" +  value[14:16] )
+    return string
+
+
+    
+sleep_time=5
+while True:
+
+    time_to_display ( get_prague_time() , ":" )
+    time.sleep(sleep_time)
+
+    string_to_display ( "$" + get_btc_price(), "" )
+    time.sleep(sleep_time)
+
+    string_to_display ( "$" + get_eth_price(), "" )
+    time.sleep(sleep_time)
+
+    string_to_display ( "$" + get_ada_price(), "" )
+    time.sleep(sleep_time)
+
+
 
