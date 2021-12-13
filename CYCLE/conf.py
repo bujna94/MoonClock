65caf659daed5ac457d03768a1deada6359b3693
Jan Dvořák <80678558+gorrdy@users.noreply.github.com>
2021-12-13 08:56:20 +0100

    Update conf.py

--- CYCLE/conf.py
+++ CYCLE/conf.py
@@ -10,22 +10,26 @@
 # D = Dogecoin
 # A = Cardano
 # P = Polkadot
-#  = 
-#  = 
 
 #Currencies
 # $ = dollar
 # € = euro
 
-"""
-conf = [
-        {'name' : 'bitcoin',    'value' : 'usd',            'prefix' : '$', 'postfix':'B',   'remove_decimal': 0, 'sleep_time' : 30 },
-        {'name' : 'ethereum',   'value' : 'eur',            'prefix' : 'E',  'postfix':'€',  'remove_decimal': 1, 'sleep_time' : 30 },
-        {'name' : 'polkadot',   'value' : 'eur',            'prefix' : '',  'postfix':'€',  'remove_decimal': 0, 'sleep_time' : 30 },
-        {'name' : 'time',       'value' : 'Europe/Prague',  'prefix' : '',  'postfix':'',   'remove_decimal': 0, 'sleep_time' : 30 },
-]
+# name = name of a cryptocurrenc. you can find this information here: https://www.coingecko.com/en/api/documentation but it's really intuitive
+# value = enter in which fiat currecny you want to get the price. for time, enter your timezone 
+# prefix = empty string or shortcut for a logo, fiat currency defined above
+# postfix = empty string or shortcut for a logo, fiat currency defined above
+# remove_decimal = 1 or 0, if 1 - removes decimals
+# sleep_time = enter value in seconds that you want to have the cryptocurrency price or time displayed
 
-"""
+#copy & paste as many lines in conf[] array as you want. No limitation for number of cryptocurrecties but you can only use logos that are defined above.
+#conf = [
+#        {'name' : 'bitcoin',    'value' : 'usd',            'prefix' : '$', 'postfix':'B',   'remove_decimal': 1, 'sleep_time' : 30 },
+#        {'name' : 'ethereum',   'value' : 'eur',            'prefix' : 'E',  'postfix':'€',  'remove_decimal': 1, 'sleep_time' : 30 },
+#        {'name' : 'polkadot',   'value' : 'eur',            'prefix' : '',  'postfix':'€',  'remove_decimal': 0, 'sleep_time' : 30 },
+#        {'name' : 'time',       'value' : 'Europe/Prague',  'prefix' : '',  'postfix':'',   'remove_decimal': 0, 'sleep_time' : 30 },
+#]
+
 
 conf = [
         {'name' : 'bitcoin',    'value' : 'usd',            'prefix' : '$', 'postfix':'',   'remove_decimal': 1, 'sleep_time' : 30 },
