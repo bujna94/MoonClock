aa2f2f4b9a1300d53cb8d80e2a5c8234478af02f
Patrik Janoušek <pj@patrikjanousek.cz>
2021-12-15 20:58:54 +0100

    Updated default config

--- CYCLE/conf.py
+++ CYCLE/conf.py
@@ -1,19 +1,6 @@
 # copy this configuration below this comment section and define your currencies, time, sleep time etc.
 # you can find more details of the supported cryptocurrencies at https://www.coingecko.com/en/api/documentation
 
-# as prefix and (or) postfix you can use cryptocurrency or currecly logo. Currently supported logos:
-# Crypto
-# B = Bitcoin
-# E = Ethereum
-# L = Litecoin
-# D = Dogecoin
-# A = Cardano
-# P = Polkadot
-
-# Currencies
-# $ = dollar
-# € = euro
-
 conf = {
     'apps': [{
         'name': 'crypto',
@@ -23,7 +10,7 @@ conf = {
     }, {
         'name': 'time',
         'timezone': 'Europe/Prague',
-        'duration': 300,
+        'duration': 600,
         'update_frequency': 30,
     }],
 }
