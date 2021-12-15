5a16347abac1368dd30e68cc7f9df68524621e4c
Patrik Janoušek <pj@patrikjanousek.cz>
2021-12-15 12:15:27 +0100

    WIP: Refactoring

---
+++ CYCLE/display.py
@@ -0,0 +1,72 @@
+import adafruit_ssd1306
+
+from font import get_symbol_for_character
+
+
+def center_string(string, capacity=10):
+    length = len(string)
+    margin = (capacity - length) // 2
+    if margin < 0:
+        margin = 0
+
+    string = (' ' * margin) + string + (' ' * margin)
+
+    if len(string) < capacity:
+        string = (' ' * (capacity - len(string))) + string
+
+    return string
+
+
+def str_rjust(string, length, char=' '):
+    while len(string) < length:
+        string = char + string
+
+    return string
+
+
+class BetterSSD1306_I2C(adafruit_ssd1306.SSD1306_I2C):
+
+    def render_symbol(self, symbol, x_offset=0):
+        size = symbol[0]
+
+        for i in range(len(symbol[1])):
+            row = i // size[0]
+            row_start_pos = row * self.width + 1
+            row_end_pos = (row + 1) * self.width - 1 + 1
+            index = row_start_pos + x_offset + (i % size[0])
+
+            if row_start_pos <= index <= row_end_pos:
+                self.buffer[index] = symbol[1][i]
+
+
+class DisplayGroup:
+
+    FIRST_CHARACTER_X_OFFSET = -5
+    SECOND_CHARACTER_X_OFFSET = 75
+
+    def __init__(self, displays):
+        self.displays = displays
+
+    def clear(self):
+        for display in self.displays:
+            display.fill(0)
+
+    def render_string(self, string, center=False, empty_as_transparent=False):
+        string = center_string(string) if center else string
+
+        for i, character in enumerate(string):
+            if i >= len(self.displays) * 2:
+                break
+
+            if character == ' ' and empty_as_transparent:
+                continue
+
+            display = self.displays[i // 2]
+            if i % 2 == 0:
+                display.render_symbol(get_symbol_for_character(character), self.FIRST_CHARACTER_X_OFFSET)
+            else:
+                display.render_symbol(get_symbol_for_character(character), self.SECOND_CHARACTER_X_OFFSET)
+
+    def show(self):
+        for display in self.displays:
+            display.show()
---
+++ CYCLE/font.py
@@ -0,0 +1,32 @@
+import symbols
+
+SYMBOL_MAP = {
+    '0': symbols.ZERO,
+    '1': symbols.ONE,
+    '2': symbols.TWO,
+    '3': symbols.THREE,
+    '4': symbols.FOUR,
+    '5': symbols.FIVE,
+    '6': symbols.SIX,
+    '7': symbols.SEVEN,
+    '8': symbols.EIGHT,
+    '9': symbols.NINE,
+    '.': symbols.DOT,
+    '$': symbols.DOLLARLARGE,
+    '€': symbols.EURO,
+    ':': symbols.LARGECOLON,
+    'B': symbols.BTC,
+    'E': symbols.ETH,
+    'L': symbols.LTC,
+    'D': symbols.DOGE,
+    'A': symbols.ADA,
+    'P': symbols.POLKADOT,
+}
+
+
+def get_symbol_for_character(character):
+
+    try:
+        return SYMBOL_MAP[character]
+    except KeyError:
+        return symbols.EMPTY
