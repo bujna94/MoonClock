861a0e51b68d9dd89301ff254e60e30f75641e26
Patrik Janoušek <pj@patrikjanousek.cz>
2021-12-18 20:20:07 +0100

    Fixed bmp_to_moonclock.py for different image modes

--- bmp_to_moonclock.py
+++ bmp_to_moonclock.py
@@ -14,7 +14,7 @@ else:
     files = [open(x, 'rb') for x in glob.glob(sys.argv[1])]
 
 for f in files:
-    img = Image.open(io.BytesIO(f.read()))
+    img = Image.open(io.BytesIO(f.read())).convert('1')
     pixels = img.load()
     symbol = []
     for i in range(img.size[1]):
