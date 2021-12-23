098024be322f2f454539de89a931299bc7262079
Jan Dvorak <dvorakh1997@gmail.com>
2021-12-23 02:38:19 +0100

    change secrets to array

--- MOONCLOCK/secrets.py
+++ MOONCLOCK/secrets.py
@@ -1,7 +1,17 @@
 # This file is where you keep secret settings, passwords, and tokens!
 # If you put them in the code you risk committing that info or sharing it
 
-secrets = {
-    'ssid': 'WIFI_SSID',
-    'password': 'WIFI_PASSWORD',
-}
+secrets = [
+    {
+        'ssid': 'WIFI_SSID1',
+        'password': 'WIFI_PASSWORD1',
+    },
+    {
+        'ssid': 'WIFI_SSID2',
+        'password': 'WIFI_PASSWORD2',
+    },
+    {
+        'ssid': 'WIFI_SSID3',
+        'password': 'WIFI_PASSWORD3',
+    },
+]
